d7cbdf5bd0e0466060544b4985e933a5b13ba16f
Ivan Ustyantsev <vangold915@gmail.com>
2024-02-01 19:05:22 +0700

    finish work with app

--- vue-todo-list/src/App.vue
+++ vue-todo-list/src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+  <div class="container d-flex flex-column text-center mt-2 mb-2">
+    <h1 class="mb-4">Modern Vuex Todo App</h1>
+    <New />
+    <div
+      v-if="message.text"
+      class="alert position-fixed top-50 start-50 translate-middle"
+      :class="messageClass"
+      role="alert"
+    >
+      {{ message.text }}
+    </div>
+    <Loader v-if="status === 'loading'" />
+    <template v-else-if="length">
+      <Stats />
+      <div class="row">
+        <Filters />
+        <List />
+        <Controls />
+      </div>
+    </template>
+    <div class="d-flex justify-content-end" v-else>
+      <Controls />
+    </div>
+  </div>
+ </template>
+ 
+ <script>
+ // Компоненты приложения
+ import New from './components/New'
+ import Stats from './components/Stats'
+ import Filters from './components/Filters'
+ import List from './components/List'
+ import Controls from './components/Controls'
+ import Loader from './components/Loader'
+ 
+ export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { New, Stats, Filters, List, Controls, Loader },
+  computed: {
+    // Вычисляем сообщение
+    message() {
+      return this.$store.state.message
+    },
+    // Вычисляем CSS-класс для сообщения
+    messageClass() {
+      return this.message.type === 'success' ? 'alert-success' : 'alert-danger'
+    },
+    // Получаем статус приложения
+    status() {
+      return this.$store.state.status
+    },
+    // Получаем длину массива с задачами
+    length() {
+      return this.$store.state.todos.length
+    }
   }
-}
-</script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+ }
+ </script>
+ 
+ <style>
+ .container {
+   max-width: 600px;
+ }
+ .alert {
+   z-index: 1;
+ }
+ </style>
--- vue-todo-list/src/main.js
+++ vue-todo-list/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+// Основной компонент приложения
 import App from './App.vue'
+// Хранилище
 import store from './store'
 
+// Получаем задачи от сервера
+store.dispatch('fetchTodos')
+
 createApp(App).use(store).mount('#app')
+
+
